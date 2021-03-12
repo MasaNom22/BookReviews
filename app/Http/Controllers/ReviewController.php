dad34f380af9f16fe7cf2cabb33cf75989023a0c
EC2 Default User <ec2-user@ip-172-31-26-197.ap-northeast-1.compute.internal>
2021-03-12 11:01:36 +0000

    レビュー投稿機能追加

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Review;
+
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -9,5 +11,20 @@ class ReviewController extends Controller
     public function index()
     {
         return view('index');
+    }
+    
+    public function create()
+    {
+        return view('review');
+    }
+    
+    public function store(Request $request)
+    {
+        $post = $request->all();
+        $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body']];
+        
+        Review::insert($data);
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,15 @@
 */
 
 Route::get('/' , 'ReviewController@index')->name('index');
+
 Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+
+Route::get('/review', 'ReviewController@create')->name('create');
+
+Route::post('/review/store', 'ReviewController@store')->name('store');
+
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
